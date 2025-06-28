d517211b04eaa4f95fdd0d9dc438df92be52c0b2
Sandeep Reddy <sandeepreddybsr1918@gmail.com>
2025-06-28 08:41:07 +0530

    Java code updated

---
+++ printname.java
@@ -0,0 +1,7 @@
+public class PrintName {
+    public static void main(String[] args) {
+        // Replace "Sandeep" with your name
+        System.out.println("Hello, my name is Sandeep!");
+    }
+}
+
